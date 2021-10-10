e2ed8d54c6c3a71015379de551b48b6822c55840
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-10-10 02:45:22 -0400

    initial

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// grab values from the inputs to get the deck size (40 cards or 60 cards) and number of lands (as well as the types of the land?)
+
+// get the number of lands in hand at the start of the game (have the user input the number) then calculate and display the chance of drawing a land
+
+// make a button that you can click every time you draw a land to get a new percent chance of drawing a land
